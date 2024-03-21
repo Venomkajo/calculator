3d260032b20ad5c791ecb6dcbaeffe5d7fdc32c6
Venomkajo <kajetanrespondek@gmail.com>
2024-03-22 00:35:49 +0100

    fixed looks, beginning javascript

---
+++ calculator.js
@@ -0,0 +1,12 @@
+var number1;
+var number2;
+var operator;
+
+
+fullKeypad = document.querySelectorAll(button);
+
+fullKeypad.forEach(key => {
+    key.addEventListener('click', function(){
+
+    });
+});
