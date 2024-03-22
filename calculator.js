81b36301d717535682c585e5d58e2321c304882f
Venomkajo <kajetanrespondek@gmail.com>
2024-03-22 01:16:04 +0100

    basic functions and button reading

--- calculator.js
+++ calculator.js
@@ -3,10 +3,29 @@ var number2;
 var operator;
 
 
-fullKeypad = document.querySelectorAll(button);
+fullKeypad = document.querySelectorAll('button');
+numberDisplay = document.querySelector('.numbers');
 
 fullKeypad.forEach(key => {
     key.addEventListener('click', function(){
-
+        numberDisplay.innerText = key.value;
     });
 });
+
+function operate(operator, num1, num2){
+    if (operator === '+'){
+        return num1 + num2;
+    }
+    else if (operator === '-'){
+        return num1 - num2;
+    }
+    else if (operator === '*'){
+        return num1 * num2;
+    }
+    else if (operator === '/'){
+        return num1 / num2;
+    }
+    else{
+        return 'ERROR';
+    }
+}
