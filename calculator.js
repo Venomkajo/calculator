676b8f0ae0b2d07258bcdac186b8dbf891c9acd8
Venomkajo <kajetanrespondek@gmail.com>
2024-03-22 01:57:50 +0100

    almost working, need fix clear

--- calculator.js
+++ calculator.js
@@ -1,7 +1,8 @@
-var number1;
-var number2;
+var number1 = '';
+var number2 = '';
 var operator = '';
 var currentNumber = '';
+var equal = 0;
 
 
 fullKeypad = document.querySelectorAll('button');
@@ -10,12 +11,29 @@ numberDisplay = document.querySelector('.numbers');
 fullKeypad.forEach(key => {
     key.addEventListener('click', function(){
         updateInput(key.value);
-        numberDisplay.innerText = currentNumber;
-    });
+        if (equal === 1){
+            equal = 0;
+            let result = operate();
+            clear();
+            numberDisplay.innerText = result;
+            number1 = result;
+        }else if (!operator){
+            numberDisplay.innerText = number1;
+        }else{
+            numberDisplay.innerText = number2;
+        }}
+    );
 });
 
-function operate(operator, num1, num2){
-    if (operator === '+'){
+function operate(){
+
+    let num1 = Number(number1);
+    let num2 = Number(number2);
+
+    if (operator === 'C'){
+        clear();
+    }    
+    else if (operator === '+'){
         return num1 + num2;
     }
     else if (operator === '-'){
@@ -33,18 +51,23 @@ function operate(operator, num1, num2){
 }
 
 function updateInput(input){
-
-    if (input === '/' || input === '*' || input === '+' || input === '-' || input === '=' || input === 'C'){
+    if (input === '/' || input === '*' || input === '+' || input === '-' || input === 'C'){
         operator = input;
-        return;
     }
-
-    if (!operator){
-        number1 = input;
-        currentNumber += number1;
+    else if (input === '='){
+        equal = 1;
+    }
+    else if (!operator){
+        number1 += input;
     }
     else{
-        number2 = input;
-        currentNumber += number2;
+        number2 += input;
     }
+}
+
+function clear(){
+    number1 = '';
+    number2 = '';
+    currentNumber = '';
+    operator = '';
 }
